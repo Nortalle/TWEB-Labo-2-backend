a7d2ff2638f41f5ec0b88e1540d32c7d7c2dd015
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-10-28 20:34:12 +0100

    first deployement

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "indent": ["error", 2],
+        "arrow-body-style": "off",
+        "arrow-parens": "off",
+        "no-console": ["error", {
+            "allow": ["warn", "error"]
+        }],
+    },
+};
---
+++ src/utils.js
@@ -0,0 +1,25 @@
+function getReposLanguagesStats(reposLanguages = []) {
+  const stats = {};
+  const countLanguages = o => {
+    Object.keys(o).forEach(key => {
+      const value = o[key];
+      const current = stats[key] || 0;
+      stats[key] = current + value;
+    });
+  };
+  reposLanguages.forEach(countLanguages);
+  return stats;
+}
+
+function getReposFiveBestLanguages(reposLanguages) {
+  const stats = [];
+  Object.keys(reposLanguages).forEach(key => {
+    stats.push({ name: key, lines: reposLanguages[key] });
+  });
+  return stats.slice(0, 5);
+}
+
+module.exports = {
+  getReposLanguagesStats,
+  getReposFiveBestLanguages,
+};
